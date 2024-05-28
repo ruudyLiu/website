7429003bab8a404c1cf8dc54b8ca6860a8816928
ruudyliu <ruudy.liu@gmail.com>
2024-05-28 01:21:18 -0700

    wip

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
+// Use https://github.com/facebook/react to build react
+// Use https://github.com/gitname/react-gh-pages to deploy to GitHub Pages
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>*Website under construction*</p>
         <p>
-          RLTest Edit <code>src/App.js</code> and save to reload.
+          RL Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
